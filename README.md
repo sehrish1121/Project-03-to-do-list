9ad724aaa63d2254a6ebdda70946af8d78df7e08
Sehrish khan <sehrishahsan1121@gmail.com>
2024-06-06 11:55:05 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Project-03-to-do-list" 
---
+++ main.ts
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+
+let todos = [];
+let condition = true;
+
+while(condition)
+
+{let todoQuestion = await inquirer.prompt([
+    {
+        name:"firstQuestion",
+        type:"input",
+        message:"what do you want to add in your Todos?"
+    },
+    {
+        name:"secondQuestion",
+        type:"confrim",
+        message:"Do you want to add more ?",
+        default: "true",
+    }
+]
+);
+todos.push(todoQuestion.firstQuestion);
+console.log(todos),
+condition = todoQuestion.secondQuestion
+
+}
